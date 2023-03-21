b00558ec2bf595c8aa4891d346ec42e7b73cf8b6
Lip-7 <zhighofficial@gmail.com>
2023-03-21 15:21:17 +0100

    minor fixes

--- js/utility.js
+++ js/utility.js
@@ -78,3 +78,27 @@ function generateElementsByArray(elementType, content, wrapper){
   singleElement.innerText = content;
   wrapper.appendChild(singleElement);
 }
+function guessingTime(correctArray) {
+  document.getElementById('btnGuess').addEventListener('click', () => {
+      switchVisibility('gameResult')
+      let counterArray = []
+      const userNums = document.querySelector('input').value.split(' ')
+      for (let numb of userNums){
+          numb = parseInt(numb)
+          if (correctArray.includes(numb)) {
+              counterArray.push(numb);
+          }
+      }
+      document.getElementById('numbersGuessed').innerHTML = counterArray.length
+      for (let i = 0; i < correctArray.length; i++) {
+          const singleNumber = document.createElement('span')
+          if (counterArray.includes(correctArray[i])) {
+              singleNumber.classList.add('correct')
+          }else{
+              singleNumber.classList.add('wrong')
+          }
+          singleNumber.innerText = correctArray[i]
+          document.getElementById('numbersEntered').appendChild(singleNumber)
+      }
+  })
+}
