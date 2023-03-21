394c9368ec31a9de1be080eeca45df0e6b18cd92
Lip-7 <zhighofficial@gmail.com>
2023-03-21 15:57:23 +0100

    fixed timer

--- js/script.js
+++ js/script.js
@@ -6,13 +6,23 @@ document.getElementById('btn-start').addEventListener('click', () => {
     for (let number of pcNumbers){
         generateElementsByArray('span', number, numsPlace)
     }
-    const countdown = setInterval(() => timing(timer, countdown), 1000)
-    setTimeout(() => {
+    const countdown = setInterval(() => {
+        if (timer > 0) {
+            timer--
+            document.getElementById('counter').innerText = timer
+        }else{
+            clearInterval(countdown)
+        }
+    }, 1000)
+    const switchTimer = setTimeout(() => {
         switchVisibility('startGame', 'main-game');
     }, 10000);
+    document.getElementById('timerReady').addEventListener('click', () => {
+        switchVisibility('startGame', 'main-game');
+        clearTimeout(switchTimer)
+    })
     guessingTime(pcNumbers);
 });
-
 function timing(timer, intervallName) {
     let counter = timer -1
     if (counter > 23) {
